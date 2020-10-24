8fe50030c27f35c5a3eb98d7c380d90da7c36a55
pedro.bazzo <pedro.bazzo@dasa.com.br>
2020-10-24 20:35:37 -0300

    passed-config

--- docs/conf.py
+++ docs/conf.py
@@ -11,11 +11,13 @@ extensions = ['sphinx.ext.autodoc',
     'sphinxtogithub']
 
 # Add any paths that contain templates here, relative to this directory.
-""" templates_path = ['templates']
+templates_path = ['templates']
 
-source_suffix = '.rst' """
-
-TEST 
+# The suffix(es) of source filenames.
+# You can specify multiple suffix as a list of string:
+#
+# source_suffix = ['.rst', '.md']
+source_suffix = '.rst'
 
 # The master toctree document.
 master_doc = 'index'
